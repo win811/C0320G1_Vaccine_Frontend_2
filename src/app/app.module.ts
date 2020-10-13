b15a48bcf1ac3482caac42889b8b0d1e4a6364ea
quandang1996 <quandanght@gmail.com>
2020-10-13 15:42:35 +0700

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BodyComponent } from './home/body/body.component';
+import { CommonModuleModule } from './common-module/common-module.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BodyComponent } from './home/body/body.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common-module/common-module.module.ts
+++ src/app/common-module/common-module.module.ts
@@ -7,6 +7,10 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CommonModuleModule { }
