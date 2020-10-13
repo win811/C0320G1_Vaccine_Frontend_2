79fe7643306c8661a780c67b6881867f2b4b66e7
NguyenDucThong <Nguyenducthong22>
2020-10-13 16:27:13 +0700

    Thong

---
+++ src/app/share/models/patient.ts
@@ -0,0 +1,12 @@
+export interface Patient {
+  id: number;
+  code: string;
+  fullName: string;
+  gender: string;
+  birthday: Date;
+  parentName: string;
+  parentIdCard: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+}
