04ca917bf0f4f68b88d35a416b8e0d1934020bb8
lamquoctung <bighero6ts@gmail.com>
2020-10-14 09:57:56 +0700

    test

---
+++ src/app/home/table-vaccice/table-vaccice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-vaccice',
+  templateUrl: './table-vaccice.component.html',
+  styleUrls: ['./table-vaccice.component.css']
+})
+export class TableVacciceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
